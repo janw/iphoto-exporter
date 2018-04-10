1edc8e17ace83abf902396aed7182eb70b8b6bc4
Jan Willhaus <mail@janwillhaus.de>
2018-04-10 16:50:58 +0200

    Remove debugging break, and add progress bar

--- iphoto-exporter.py
+++ iphoto-exporter.py
@@ -3,6 +3,7 @@
 import plistlib
 from os import path, makedirs
 from shutil import copyfile
+from tqdm import tqdm
 
 ALBUM_FILENAME = 'AlbumData.xml'
 INPUT_DIRECTORY = '/mnt/home/Pictures/iPhoto/iPhoto Library'
@@ -15,7 +16,7 @@ with open(path.join(INPUT_DIRECTORY, ALBUM_FILENAME), 'rb') as fp:
     data = plistlib.load(fp)
     masters = data.get('Master Image List')
 
-for album in data['List of Albums']:
+for album in tqdm(data['List of Albums']):
     if album['GUID'] not in ['allPhotosAlbum', 'flaggedAlbum', 'lastNMonthsAlbum', 'lastImportAlbum', ]:
         album_directory = path.join(OUTPUT_DIRECTORY, album['AlbumName'])
         makedirs(album_directory, exist_ok=True)
@@ -34,10 +35,8 @@ for album in data['List of Albums']:
 
             full_source_path = path.join(INPUT_DIRECTORY, 'Masters', image_path)
             full_dest_path = path.join(album_directory, photo_key + '__' + image_filename)
-            print(full_source_path)
-            print(full_dest_path)
-            copyfile(full_source_path, full_dest_path)
 
-
-        break
-
+            try:
+                copyfile(full_source_path, full_dest_path)
+            except FileNotFoundError:
+                pass
