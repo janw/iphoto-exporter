97b422efa33e0bde70186e5f83894b7e47fd6598
Jan Willhaus <mail@janwillhaus.de>
2018-04-10 18:11:10 +0200

    Move all logic into functions

--- iphoto_exporter.py
+++ iphoto_exporter.py
@@ -7,23 +7,39 @@ from shutil import copyfile
 from slugify import slugify
 from tqdm import tqdm
 
-ALBUM_FILENAME = 'AlbumData.xml'
-INPUT_DIRECTORY = '/mnt/home/Pictures/iPhoto/iPhoto Library'
-OUTPUT_DIRECTORY = '/mnt/home/Pictures/iPhotoExport'
+XML_FILENAME = 'AlbumData.xml'
 IGNORED_ALBUMS = ['flaggedAlbum', 'lastNMonthsAlbum', 'lastImportAlbum', ]
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Export master images from an iPhoto Library.')
+    parser.add_argument('-i', '--input', required=True,
+        help='''Path to the iPhoto Library to be exported''')
+    parser.add_argument('-o', '--output', required=True,
+        help='''Base of the output path to which the albums are exported''')
+    parser.add_argument('-a', '--all', action='store_true',
+        help='''If set, an album containing all photos will be created as well''')
+
+    args = parser.parse_args()
+    export_iphoto(args)
 
 
 def export_iphoto(args):
 
-    with open(path.join(INPUT_DIRECTORY, ALBUM_FILENAME), 'rb') as fp:
+    with open(path.join(args.input, XML_FILENAME), 'rb') as fp:
         data = plistlib.load(fp)
         masters = data.get('Master Image List')
+
+    ignored_albums = IGNORED_ALBUMS
+    if not args.all:
+        ignored_albums.append('allPhotosAlbum')
 
     albums = [a for a in data['List of Albums']
               if a['GUID'] not in IGNORED_ALBUMS]
 
     for album in tqdm(albums):
-        album_directory = path.join(OUTPUT_DIRECTORY, slugify(album['AlbumName']))
+        album_directory = path.join(args.output, slugify(album['AlbumName']))
         makedirs(album_directory, exist_ok=True)
 
         for photo_key in album['KeyList']:
@@ -39,7 +55,7 @@ def export_iphoto(args):
             image_filename = path.basename(image_path)
 
             full_source_path = path.join(
-                INPUT_DIRECTORY, 'Masters', image_path)
+                args.input, 'Masters', image_path)
             full_dest_path = path.join(
                 album_directory, slugify(photo_key + '__' + image_filename))
 
@@ -49,22 +65,5 @@ def export_iphoto(args):
                 pass
 
 
-parser = argparse.ArgumentParser(
-    description='Export master images from an iPhoto Library.')
-parser.add_argument('-i', '--input', required=True,
-    help='''Path to the iPhoto Library to be exported''')
-parser.add_argument('-o', '--output', required=True,
-    help='''Base of the output path to which the albums are exported''')
-parser.add_argument('-a', '--all', action='store_true',
-    help='''If set, an album containing all photos will be created as well''')
-
 if __name__ == '__main__':
-    args = parser.parse_args()
-
-    if not args.all:
-        IGNORED_ALBUMS.append('allPhotosAlbum')
-
-    INPUT_DIRECTORY = args.input
-    OUTPUT_DIRECTORY = args.output
-
-    export_iphoto(args)
+    main()
